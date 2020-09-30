6c9cc30b21d232b07ac5b214129c22f9edc3248b
Donna Torres <todonnatorres@icloud.com>
2020-09-29 23:00:27 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :phone_number, length: { minimum: 10 }
+    validates :name, uniqueness: true
+    
 end
+
+author = Author.new
+author.errors.messages 
+author.valid? 
+author.errors.messages
+
